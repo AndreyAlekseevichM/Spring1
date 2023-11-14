202881f86b54025848ec15a93e1e4383a4ceaf64
Andrey <89024406064@mail.ru>
2023-11-15 00:53:06 +0500

    HTTP и современный Web

---
+++ src/main/java/Request.java
@@ -0,0 +1,86 @@
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Request {
+
+    private final RequestLine requestLine;
+    private final List<String> headers;
+    private final List<NameValuePair> queryParams;
+    private String body;
+    private List<NameValuePair> postParams;
+
+    public Request(RequestLine requestLine, List<String> headers) throws URISyntaxException {
+        this.requestLine = requestLine;
+        this.headers = headers;
+        queryParams = URLEncodedUtils.parse(new URI(requestLine.getPath()), StandardCharsets.UTF_8);
+    }
+
+    public RequestLine getRequestLine() {
+        return requestLine;
+    }
+
+    public Optional<String> getHeader(String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    public List<String> getHeaders() {
+        return headers;
+    }
+
+    public List<String> getQueryParam(String name) {
+        return getParam(queryParams, name);
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public List<String> getPostParam(String name) {
+        return getParam(postParams, name);
+    }
+
+    public List<NameValuePair> getPostParams() {
+        return postParams;
+    }
+
+    public void setPostParams(List<NameValuePair> postParams) {
+        this.postParams = postParams;
+    }
+
+    private List<String> getParam(List<NameValuePair> params, String name) {
+        return params.stream()
+                .filter(o -> o.getName().startsWith(name))
+                .map(NameValuePair::getValue)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "requestLine=" + requestLine +
+                ", headers='" + headers + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,199 @@
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+
+    private final Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
+
+    public void listen(int port) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final Socket socket = serverSocket.accept();
+                threadPool.submit(new SocketHandler(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        Map<String, Handler> map = new ConcurrentHashMap<>();
+        if (handlers.containsKey(method)) {
+            map = handlers.get(method);
+        }
+        map.put(path, handler);
+        handlers.put(method, map);
+    }
+
+    public void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    public void notFound(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    private class SocketHandler implements Runnable {
+
+        private final Socket socket;
+
+        private final byte[] requestLineDelimiter = new byte[]{'\r', '\n'};
+        private final byte[] headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+
+
+        public SocketHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                    final BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+                    final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())
+            ) {
+                // лимит на request line + заголовки
+                final int limit = 4096;
+
+                in.mark(limit);
+                final byte[] buffer = new byte[limit];
+                final int read = in.read(buffer);
+
+                RequestLine requestLine = getRequestLine(buffer, read);
+                if (requestLine == null) {
+                    badRequest(out);
+                    return;
+                }
+
+                List<String> headers = getHeaders(buffer, read, in);
+                if (headers == null) {
+                    badRequest(out);
+                    return;
+                }
+
+                Request request = new Request(requestLine, headers);
+                request.setBody(getBody(request, in));
+                request.setPostParams(getPostParams(request));
+
+                runHandler(request, out);
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private RequestLine getRequestLine(byte[] buffer, int read) {
+            // ищем request line
+            final int requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+            if (requestLineEnd == -1) {
+                return null;
+            }
+            // читаем request line
+            final String[] parts = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+            if (parts.length != 3) {
+                return null;
+            }
+            // проверяем, валидность пути
+            if (!parts[1].startsWith("/")) {
+                return null;
+            }
+            // получили request line
+            return new RequestLine(parts[0], parts[1], parts[2]);
+        }
+
+        private List<String> getHeaders(byte[] buffer, int read, BufferedInputStream in) throws IOException {
+            final int requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+            // ищем заголовки
+            final int headersStart = requestLineEnd + requestLineDelimiter.length;
+            final int headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+            if (headersEnd == -1) {
+                return null;
+            }
+            // отматываем на начало буфера
+            in.reset();
+            // пропускаем requestLine
+            in.skip(headersStart);
+            // получили заголовки
+            final byte[] headersBytes = in.readNBytes(headersEnd - headersStart);
+            return Arrays.asList(new String(headersBytes).split("\r\n"));
+        }
+
+        private String getBody(Request request, BufferedInputStream in) throws IOException {
+
+            if (!request.getRequestLine().getMethod().equals("GET")) {
+                in.skip(headersDelimiter.length);
+                final Optional<String> contentLength = request.getHeader("Content-Length");
+                if (contentLength.isPresent()) {
+                    final int length = Integer.parseInt(contentLength.get());
+                    final byte[] bodyBytes = in.readNBytes(length);
+                    return new String(bodyBytes);
+                }
+            }
+            return null;
+        }
+
+        private List<NameValuePair> getPostParams(Request request) {
+            if (!request.getRequestLine().getMethod().equals("GET")) {
+                final Optional<String> contentType = request.getHeader("Content-Type");
+                if (contentType.isPresent()) {
+                    final String type = contentType.get();
+                    if (type.equals("application/x-www-form-urlencoded")) {
+                        return URLEncodedUtils.parse(request.getBody(), StandardCharsets.UTF_8);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void runHandler(Request request, BufferedOutputStream out) throws IOException {
+            Handler handler = handlers.get(request.getRequestLine().getMethod())
+                    .get(request.getRequestLine().getPath());
+
+            if (handler != null) {
+                handler.handle(request, out);
+            } else {
+                notFound(out);
+            }
+        }
+
+        private int indexOf(byte[] array, byte[] target, int start, int max) {
+            outer:
+            for (int i = start; i < max - target.length + 1; i++) {
+                for (int j = 0; j < target.length; j++) {
+                    if (array[i + j] != target[j]) {
+                        continue outer;
+                    }
+                }
+                return i;
+            }
+            return -1;
+        }
+    }
+}
